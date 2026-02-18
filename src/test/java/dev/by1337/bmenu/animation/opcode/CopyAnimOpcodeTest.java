0f8fde773c84e4045d5580a1c9e02f1b7a60823a
by1337 <by133700@gmail.com>
2026-02-18 10:02:19 +0300

    release: 2.0

---
+++ src/test/java/dev/by1337/bmenu/animation/opcode/CopyAnimOpcodeTest.java
@@ -0,0 +1,23 @@
+package dev.by1337.bmenu.animation.opcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class CopyAnimOpcodeTest {
+
+    @Test
+    void parseTest() throws ParseException {
+        assertArrayEquals(new int[]{10, 11, 12}, new CopyAnimOpcode(("10,11,12 10-15")).getSrc());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new CopyAnimOpcode(("10 10-12,17")).getDest());
+        assertArrayEquals(new int[]{10, 11, 12, 17, 18, 19}, new CopyAnimOpcode(("10 10-12,17-19")).getDest());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new CopyAnimOpcode(("10-12,17 10-12,17")).getSrc());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new CopyAnimOpcode(("10-12,17 10-12,17")).getDest());
+        assertArrayEquals(new int[]{10}, new CopyAnimOpcode(("10++")).getSrc());
+        assertArrayEquals(new int[]{11}, new CopyAnimOpcode(("10++")).getDest());
+        assertArrayEquals(new int[]{10}, new CopyAnimOpcode(("10--")).getSrc());
+        assertArrayEquals(new int[]{9}, new CopyAnimOpcode(("10--")).getDest());
+    }
+}
---
+++ src/test/java/dev/by1337/bmenu/animation/opcode/MoveAnimOpcodeTest.java
@@ -0,0 +1,16 @@
+package dev.by1337.bmenu.animation.opcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class MoveAnimOpcodeTest {
+    @Test
+    void parseTest() {
+        assertArrayEquals(new int[]{10, 11, 12}, new MoveAnimOpcode(("10,11,12 10-15")).getFrom());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new MoveAnimOpcode(("10 10-12,17")).getTo());
+        assertArrayEquals(new int[]{10, 11, 12, 17, 18, 19}, new MoveAnimOpcode(("10 10-12,17-19")).getTo());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new MoveAnimOpcode(("10-12,17 10-12,17")).getFrom());
+        assertArrayEquals(new int[]{10, 11, 12, 17}, new MoveAnimOpcode(("10-12,17 10-12,17")).getTo());
+    }
+}
